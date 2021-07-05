ca56f548cabc80b77812b7fa05a97636273bb4e7
Abdul Rahmad Pasaribu <abdoelrachmad@gmail.com>
2021-07-05 11:31:35 +0700

    add basic project setup created by android studio 4.2.1

---
+++ android/app/src/main/java/io/github/misterabdul/FlutterAndroidSampleApp/flutter_android_sample_app/MainActivity.java
@@ -0,0 +1,6 @@
+package io.github.misterabdul.FlutterAndroidSampleApp.flutter_android_sample_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
